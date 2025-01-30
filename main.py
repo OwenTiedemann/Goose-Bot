0e97bad34e730e5578412fd6cee8ab6ae6733639
Owen Tiedemann <84743529+OwenTiedemann@users.noreply.github.com>
2025-01-29 21:53:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ async def on_message(message):
 
     random_value = random.randrange(1, 100)
 
+    await client.process_commands(msg)
+
     #if 1 == random_value:
     # await message.reply('This message has been randomly selected, please pay $10 before proceeding.')
 
